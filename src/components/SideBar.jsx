07aeb4664ae8465271cd7b3528fde08a5b4f26a7
Abimael <abimael.spdev@gmail.com>
2025-01-07 08:41:33 -0400

    feat: add SideBar component

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,20 @@
+import CustomButton from './CustomButton'
+
+import './SideBar.scss'
+import logo from '../assets/images/1111.png'
+
+const SideBar = () => {
+    return ( 
+        <div className="sidebar-container">
+            <div className="logo">
+                <img src={logo} alt="FUll Stack Club" />
+            </div>
+
+            <div className="sign-out">
+                <CustomButton/>
+            </div>
+        </div>
+     );
+}
+ 
+export default SideBar;
